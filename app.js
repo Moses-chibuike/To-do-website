f0431d6d5c72f3f45b35084f0765b44b6904fb54
zeusmoney <ezechukwuchibuikemoses@gmail.com>
2022-12-13 12:46:20 +0000

    additions

--- app.js
+++ app.js
@@ -1,16 +1,48 @@
-//jshint esversion: 6
+//jshint esversion:6
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
 const app = express();
 
-app.get("/", (req, res) => {
+app.set('view engine', 'ejs');
 
-  res.send("Hello World");
-  });
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
 
+app.get("/", function(req, res) {
 
-app.listen(3000, function(){
-    console.log("server started on port 3000");
-  });
+const day = date.getDate();
+
+  res.render("list", {listTitle: day, newListItems: items});
+
+});
+
+app.post("/", function(req, res){
+
+  const item = req.body.newItem;
+
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", function(req,res){
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.get("/about", function(req, res){
+  res.render("about");
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
